7bf1dc060c39580684b5506538e10eb051910c74
hayden <hayden@localhost>
2013-12-18 08:19:03 +1100

    missed a bit in last commit

--- KV_Team_OSD/GlobalVariables.h
+++ KV_Team_OSD/GlobalVariables.h
@@ -216,6 +216,9 @@ int16_t I2CError=0;
 uint16_t cycleTime=0;
 uint16_t pMeterSum=0;
 uint16_t MwRssi=0;
+int32_t GPS_time = 0;        //local time of coord calc - haydent
+int16_t GPS_tz = 0;
+int8_t GPS_dst = 0;
 
 // For decoration
 uint8_t SYM_AH_DECORATION_LEFT = 0x10;
