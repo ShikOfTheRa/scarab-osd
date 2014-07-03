9259180c1d683f0165d14050c30ba60e123455ad
hayden <hayden@localhost>
2014-07-04 09:41:17 +1000

    sport bug correction

---
+++ MW_OSD/Config.h
@@ -0,0 +1,160 @@
+/*--------------------------       configurable parameters      ----------------------------------------------------*/
+
+
+
+/********************       FEATURES      *********************/
+// Currently there is not enough memory to run all options
+// Disable features if you require memory for other features
+// Further configuration may be require elsewhere in config.h + option enabled on GUI
+#define SBDIRECTION     // Enable/disable sidebar indicators (changes in speed or altitude)
+#define HORIZON         // Enable/disable HORIZON indicator
+#define MAPMODE         // Enable/disable MAP MODE - map indication of relative positions of home and aircraft
+#define GPSTIME         // Enable/disable GPS Time functions
+#define SPORT           // Enable/disable FRSKY S.PORT cell code
+
+
+/********************       HARDWARE OSD settings      *********************/
+//#define USE_VSYNC                 // Remove "sparklies" on boards that support VSYNC 
+
+
+/********************       HARDWARE CURRENT sensor settings      *********************/
+#define AMPERAGEMAX     500         // Size of current sensor / maximum current draw (* 10) e.g. 50A sensor = 500, 100A sensor = 1000
+#define AMPERAGEOFFSET  0           // Optional extra for high offset sensors not supported in GUI (typically bidirectional sensors use a value of 256-512) 
+
+
+/********************       FILTER settings      *********************/
+//#define STAGE2FILTER              // Enable for smoother readings of voltage / current / RSSI. 
+//#define SMOOTHFILTER              // Enable for smoothest readings of voltage / current / RSSI. Uses more memory. NOT FINISHED
+
+
+/********************       CONTROLLER settings      *********************/
+//#define BASEFLIGHT                // Undefine this if you are using BASEFLIGHT / others to correct for incorrect heading
+//#define FIXEDWING                 // Undefine this if you are using MW fixed wing from PatrikE - to use GPS heading and altitude instead of BARO/MAG
+//#define FASTPWMRSSI               // Undefine this if you are using non standard PWM for RSSI ( doubles resolution ) 
+
+
+/********************       CALLSIGN settings      *********************/
+//#define CALLSIGNSTARTUP           // Enable to display callsign at startup
+//#define CALLSIGNALWAYS  341       // Enable to permanently display callsign. Number = screen position (row*30 + column)
+//#define FREETEXTLLIGHTS 342       // Enable to display freetext (or callsign) when LLIGHTS Switch active on TX. Number = screen position (row*30 + column)
+//#define FREETEXTGIMBAL  342       // Enable to display freetext (or callsign) when GIMBAL Switch active on TX. Number = screen position (row*30 + column)
+
+
+/********************       GPS type settings      *********************/
+//#define I2CGPS_SPEED              // Undefine this if you are using older I2CGPS - and need to correct for speed error (10x too slow)               
+//#define I2CGPS_DISTANCE           // Undefine this if you are using older I2CGPS - and need to correct for distance error (650m max) UNTESTED               
+
+
+/********************       MAP MODE Settings       *********************/
+#define MAPTYPE 1                   // 1 for MAP - home is center, map shows location of aircraft relative to home 
+                                    // 0 for RADAR - home is aircraft, map shows location of home relative to aircraft 
+//#define MAPMODENORTH              // Enable to use North as MAP reference in MODE 1 instead of take off direction (Default = disable) 
+
+
+/********************       FrSky S.Port settings      *********************/
+//enables data transfer from frsky reciever s.port to osd via multiwii
+//requires serial inverter cable & multiwii with s.port code
+//Auto detected cell graph from s.port, 16 steps @ 0.05v 
+//To show battery voltage from s.port, enable "Use MWii" under "Main Voltage" in GUI
+//To show amperage from s.port, enable "Use MWii" under Amperage in GUI
+//more details: http://code.google.com/p/scarab-osd/wiki/Frsky_SPort
+#define MIN_CELL 320 //Cell Low Flash - No decimal, 3 Digits ie 320 = 3.20v
+
+
+/********************       Display Settings         ************************/
+#define DECIMAL '.'                 // Decimal point character, change to what suits you best (.) (,)
+//#define SHIFTDOWN                 // Select if your monitor cannot display top line fully. It shifts some lines down
+//#define ALT_CENTER                // Enable alternative center crosshair
+//#define HIDEARMEDSTATUS           // Enable to hide ARMED / DISARMED status
+//#define FASTPIXEL                 // Optional - may improve resolution - especially hi res cams
+//#define WHITEBRIGHTNESS 0x00      // Optional change from default 0x00=120%,0x10=90%0x11=80%  default is 0x01=100%,
+//#define INTRO_DELAY 5             // Seconds intro screen should show for. Default is 10 
+#define TIMEZONESTARTUP             // Enable to display timezone at startup - if GPS TIME is enabled
+
+
+/********************   ENABLE/DISABLE CONFIG PAGES via STICK MENU     *********************/
+//large memory savings if not needed, comment to disable
+#define PAGE1 //PID CONFIG
+#define PAGE2 //RC TUNING
+#define PAGE3 //VOLTAGE
+#define PAGE4 //RSSI
+#define PAGE5 //CURRENT
+#define PAGE6 //DISPLAY
+#define PAGE7 //ADVANCED
+#define PAGE8 //GPS TIME
+
+
+/********************  LEGACY compatibility **********************/
+//#define BOXNAMES                  // required to support multiwii 2.1 / 2.0 and HARIKIRI
+
+
+/*--------------------------       advanced parameters      ----------------------------------------------------*/
+/*--------------------------       advanced parameters      ----------------------------------------------------*/
+/*--------------------------       advanced parameters      ----------------------------------------------------*/
+/*--------------------------       advanced parameters      ----------------------------------------------------*/
+
+/********************       ADVANCED HARDWARE settings      *********************/
+//#define RUSHDUINO                 // Select board type - RUSHDUINO. Defaults to MINIM
+//#define TEMPSENSOR                // Enable if you have a hardware temperature sensor
+#define STARTUPDELAY                // Enable startup delay of 1 sec to allow MAX chip voltage to rise fully and initialise before configuring 
+#define DIVIDER1v1      0.0001      // Voltage divider for 1.1v reference. Use 0.0001 default unless advised otherwise.
+#define DIVIDER5v       0.0005      // Voltage divider for 5v reference. Use 0.0005 default unless advised otherwise.
+
+
+/********************       HARDWARE PINS settings      *********************/
+#define VOLTAGEPIN    A0
+#define VIDVOLTAGEPIN A2
+#define AMPERAGEPIN   A1
+#define TEMPPIN       A6           
+#define RSSIPIN       A3              
+#define PWMRSSIPIN    A3              
+#define LEDPIN        7
+#ifdef RUSHDUINO                     //RUSHDUINO
+    # define MAX7456SELECT 10        // ss 
+    # define MAX7456RESET  9         // RESET
+#else                                // MINIM  
+    # define MAX7456SELECT 6         // ss
+    # define MAX7456RESET  10        // RESET
+#endif
+
+
+/********************       For OSD SWITCH      *********************/
+// In following defines, use DISPLAY_MIN_OFF to hide when using OSD_SWITCH
+// Otherwise select DISPLAY_ALWAYS (it can be enabled / disbled by GUI
+
+#define  OSDOFF01 DISPLAY_MIN_OFF // GPS_numSatPosition
+#define  OSDOFF02 DISPLAY_MIN_OFF // GPS_numSatPositionTop      // On top of screen
+#define  OSDOFF03 DISPLAY_MIN_OFF // GPS_directionToHomePosition
+#define  OSDOFF04 DISPLAY_MIN_OFF // GPS_distanceToHomePosition
+#define  OSDOFF05 DISPLAY_MIN_OFF // speedPosition
+#define  OSDOFF06 DISPLAY_MIN_OFF // GPS_angleToHomePosition
+#define  OSDOFF07 DISPLAY_MIN_OFF // MwGPSAltPosition
+#define  OSDOFF08 DISPLAY_MIN_OFF // sensorPosition
+#define  OSDOFF09 DISPLAY_MIN_OFF // MwHeadingPosition
+#define  OSDOFF10 DISPLAY_MIN_OFF // MwHeadingGraphPosition
+#define  OSDOFF11 DISPLAY_MIN_OFF // MwAltitudePosition
+#define  OSDOFF12 DISPLAY_MIN_OFF // MwClimbRatePosition
+#define  OSDOFF13 DISPLAY_MIN_OFF // CurrentThrottlePosition
+#define  OSDOFF14 DISPLAY_ALWAYS // flyTimePosition
+#define  OSDOFF15 DISPLAY_ALWAYS // onTimePosition
+#define  OSDOFF16 DISPLAY_ALWAYS // motorArmedPosition
+#define  OSDOFF17 DISPLAY_MIN_OFF  // MwGPSLatPosition
+#define  OSDOFF18 DISPLAY_MIN_OFF  // MwGPSLonPosition
+#define  OSDOFF19 DISPLAY_MIN_OFF  // MwGPSLatPositionTop      // On top of screen
+#define  OSDOFF20 DISPLAY_MIN_OFF  // MwGPSLonPositionTop      // On top of screen
+#define  OSDOFF21 DISPLAY_MIN_OFF // rssiPosition
+#define  OSDOFF22 DISPLAY_MIN_OFF // temperaturePosition
+#define  OSDOFF23 DISPLAY_ALWAYS // voltagePosition
+#define  OSDOFF24 DISPLAY_MIN_OFF // vidvoltagePosition
+#define  OSDOFF25 DISPLAY_MIN_OFF // amperagePosition
+#define  OSDOFF26 DISPLAY_MIN_OFF // pMeterSumPosition
+#define  OSDOFF27 DISPLAY_MIN_OFF  // horizonPosition
+#define  OSDOFF28 DISPLAY_MIN_OFF // CallSign Position
+#define  OSDOFF29 DISPLAY_ALWAYS // Debug Position
+//#define  OSDOFF30 DISPLAY_MIN_OFF // Gimbal Position
+#define  OSDOFF31 DISPLAY_MIN_OFF // GPS_time Position
+
+
+/*----------------------------------------------       Developer parameters      ----------------------------------------------------*/
+//#define DEBUG         // Enable/disable option to display Mutltiwii debug values 
+
---
+++ MW_OSD/Max7456.ino
@@ -0,0 +1,325 @@
+#define DATAOUT 11              // MOSI
+#define DATAIN  12              // MISO
+#define SPICLOCK  13            // sck
+
+#define VSYNC 2                 // INT0
+
+#ifndef WHITEBRIGHTNESS
+  #define WHITEBRIGHTNESS 0x01
+#endif
+
+//MAX7456 opcodes
+#define DMM_reg   0x04
+#define DMAH_reg  0x05
+#define DMAL_reg  0x06
+#define DMDI_reg  0x07
+#define VM0_reg   0x00
+#define VM1_reg   0x01
+
+// video mode register 0 bits
+#define VIDEO_BUFFER_DISABLE 0x01
+//#define MAX7456_RESET 0x02
+#define VERTICAL_SYNC_NEXT_VSYNC 0x04
+#define OSD_ENABLE 0x08
+#define SYNC_MODE_AUTO 0x00
+#define SYNC_MODE_INTERNAL 0x30
+#define SYNC_MODE_EXTERNAL 0x20
+#define VIDEO_MODE_PAL 0x40
+#define VIDEO_MODE_NTSC 0x00
+
+// video mode register 1 bits
+
+
+// duty cycle is on_off
+#define BLINK_DUTY_CYCLE_50_50 0x00
+#define BLINK_DUTY_CYCLE_33_66 0x01
+#define BLINK_DUTY_CYCLE_25_75 0x02
+#define BLINK_DUTY_CYCLE_75_25 0x03
+
+// blinking time
+#define BLINK_TIME_0 0x00
+#define BLINK_TIME_1 0x04
+#define BLINK_TIME_2 0x08
+#define BLINK_TIME_3 0x0C
+
+// background mode brightness (percent)
+#define BACKGROUND_BRIGHTNESS_0 0x00
+#define BACKGROUND_BRIGHTNESS_7 0x01
+#define BACKGROUND_BRIGHTNESS_14 0x02
+#define BACKGROUND_BRIGHTNESS_21 0x03
+#define BACKGROUND_BRIGHTNESS_28 0x04
+#define BACKGROUND_BRIGHTNESS_35 0x05
+#define BACKGROUND_BRIGHTNESS_42 0x06
+#define BACKGROUND_BRIGHTNESS_49 0x07
+
+#define BACKGROUND_MODE_GRAY 0x40
+
+//MAX7456 commands
+#define CLEAR_display 0x04
+#define CLEAR_display_vert 0x06
+#define END_string 0xff
+
+
+#define MAX7456ADD_VM0          0x00  //0b0011100// 00 // 00             ,0011100
+#define MAX7456ADD_VM1          0x01
+#define MAX7456ADD_HOS          0x02
+#define MAX7456ADD_VOS          0x03
+#define MAX7456ADD_DMM          0x04
+#define MAX7456ADD_DMAH         0x05
+#define MAX7456ADD_DMAL         0x06
+#define MAX7456ADD_DMDI         0x07
+#define MAX7456ADD_CMM          0x08
+#define MAX7456ADD_CMAH         0x09
+#define MAX7456ADD_CMAL         0x0a
+#define MAX7456ADD_CMDI         0x0b
+#define MAX7456ADD_OSDM         0x0c
+#define MAX7456ADD_RB0          0x10
+#define MAX7456ADD_RB1          0x11
+#define MAX7456ADD_RB2          0x12
+#define MAX7456ADD_RB3          0x13
+#define MAX7456ADD_RB4          0x14
+#define MAX7456ADD_RB5          0x15
+#define MAX7456ADD_RB6          0x16
+#define MAX7456ADD_RB7          0x17
+#define MAX7456ADD_RB8          0x18
+#define MAX7456ADD_RB9          0x19
+#define MAX7456ADD_RB10         0x1a
+#define MAX7456ADD_RB11         0x1b
+#define MAX7456ADD_RB12         0x1c
+#define MAX7456ADD_RB13         0x1d
+#define MAX7456ADD_RB14         0x1e
+#define MAX7456ADD_RB15         0x1f
+#define MAX7456ADD_OSDBL        0x6c
+#define MAX7456ADD_STAT         0xA0
+
+// Selectable by video mode
+//uint8_t ENABLE_display;
+//uint8_t ENABLE_display_vert;
+//uint8_t DISABLE_display;
+uint16_t MAX_screen_size;
+
+//////////////////////////////////////////////////////////////
+uint8_t spi_transfer(uint8_t data)
+{
+  SPDR = data;                    // Start the transmission
+  while (!(SPSR & (1<<SPIF)))     // Wait the end of the transmission
+    ;
+  return SPDR;                    // return the received byte
+}
+
+// ============================================================   WRITE TO SCREEN
+
+void MAX7456Setup(void)
+{
+  uint8_t MAX7456_reset;
+  uint8_t MAX_screen_rows;
+
+ 
+  if(Settings[S_VIDEOSIGNALTYPE]) {    // PAL
+    //ENABLE_display = 0x48;
+    //ENABLE_display_vert = 0x4c;
+    MAX7456_reset = 0x42;
+    //DISABLE_display = 0x40;
+    MAX_screen_size = 480;
+    MAX_screen_rows = 16;
+  }
+  else {                                // NTSC
+    //ENABLE_display = 0x08;
+    //ENABLE_display_vert = 0x0c;
+    MAX7456_reset = 0x02;
+    //DISABLE_display = 0x00;
+    MAX_screen_size = 390;
+    MAX_screen_rows = 13;
+  }
+
+  pinMode(MAX7456RESET,OUTPUT);
+  digitalWrite(MAX7456RESET,HIGH); //hard enable
+
+  delay(250);
+
+  pinMode(MAX7456SELECT,OUTPUT);
+  digitalWrite(MAX7456SELECT,HIGH); //disable device
+
+  pinMode(DATAOUT, OUTPUT);
+  pinMode(DATAIN, INPUT);
+  pinMode(SPICLOCK,OUTPUT);
+  pinMode(VSYNC, INPUT);
+
+  // SPCR = 01010000
+  //interrupt disabled,spi enabled,msb 1st,master,clk low when idle,
+  //sample on leading edge of clk,system clock/4 rate (4 meg)
+
+  SPCR = (1<<SPE)|(1<<MSTR);
+  uint8_t spi_junk;
+  spi_junk=SPSR;
+  spi_junk=SPDR;
+  delay(250);
+
+  // force soft reset on Max7456
+  digitalWrite(MAX7456SELECT,LOW);
+  MAX7456_Send(VM0_reg, MAX7456_reset);
+  delay(500);
+
+#ifdef FASTPIXEL 
+  // force fast pixel timing
+  MAX7456_Send(MAX7456ADD_OSDM, 0x00);
+  // MAX7456_Send(MAX7456ADD_OSDM, 0xEC);
+  // uint8_t srdata = spi_transfer(0xFF); //get data byte
+  // srdata = srdata & 0xEF;
+  // MAX7456_Send(0x6c, srdata);
+  delay(500);
+#endif
+
+  // set all rows to same charactor white level, 120%
+  uint8_t x;
+  for(x = 0; x < MAX_screen_rows; x++) {
+    MAX7456_Send(MAX7456ADD_RB0+x, WHITEBRIGHTNESS);
+  }
+
+  // make sure the Max7456 is enabled
+  spi_transfer(VM0_reg);
+
+  if (Settings[S_VIDEOSIGNALTYPE]){
+    spi_transfer(OSD_ENABLE|VIDEO_MODE_PAL);
+  }
+  else{
+    spi_transfer(OSD_ENABLE|VIDEO_MODE_NTSC);
+  }
+  digitalWrite(MAX7456SELECT,HIGH);
+  delay(100);
+# ifdef USE_VSYNC
+  EIMSK |= (1 << INT0);  // enable interuppt
+  EICRA |= (1 << ISC01); // interrupt at the falling edge
+  sei();
+#endif
+}
+
+// Copy string from ram into screen buffer
+void MAX7456_WriteString(const char *string, int Adresse)
+{
+  uint8_t xx;
+  for(xx=0;string[xx]!=0;)
+  {
+    screen[Adresse++] = string[xx++];
+  }
+}
+
+// Copy string from progmem into the screen buffer
+void MAX7456_WriteString_P(const char *string, int Adresse)
+{
+  uint8_t xx = 0;
+  char c;
+  while((c = (char)pgm_read_byte(&string[xx++])) != 0)
+  {
+    screen[Adresse++] = c;
+  }
+}
+
+#ifdef USE_VSYNC
+volatile unsigned char vsync_wait = 0;
+  ISR(INT0_vect) {
+  vsync_wait = 0;
+}
+#endif
+void MAX7456_DrawScreen()
+{
+#ifdef USE_VSYNC
+  vsync_wait = 1;
+  while (vsync_wait)
+  ;
+#endif
+int xx;
+#ifdef USE_VSYNC
+  digitalWrite(MAX7456SELECT,LOW);
+  spi_transfer(DMM_reg);
+  spi_transfer(1);
+  spi_transfer(DMAH_reg);
+  spi_transfer(0);
+  spi_transfer(DMAL_reg);
+  spi_transfer(0);
+#else
+digitalWrite(MAX7456SELECT,LOW);
+#endif
+
+  digitalWrite(MAX7456SELECT,LOW);
+  for(xx=0;xx<MAX_screen_size;++xx)
+  {
+#ifndef USE_VSYNC
+    MAX7456_Send(MAX7456ADD_DMAH, xx>>8);
+    MAX7456_Send(MAX7456ADD_DMAL, xx);
+#endif
+    MAX7456_Send(MAX7456ADD_DMDI, screen[xx]);
+    screen[xx] = ' ';
+  }
+#ifdef USE_VSYNC
+  spi_transfer(DMDI_reg);
+  spi_transfer(END_string);
+  spi_transfer(DMM_reg);
+  spi_transfer(B00000000);
+#endif
+  digitalWrite(MAX7456SELECT,HIGH);
+}
+
+void MAX7456_Send(uint8_t add, uint8_t data)
+{
+  spi_transfer(add);
+  spi_transfer(data);
+}
+
+
+//MAX7456 commands
+
+#define WRITE_TO_MAX7456
+#define NVM_ram_size 54
+
+#define CLEAR_display 0x04
+#define CLEAR_display_vert 0x06
+#define END_string 0xff
+#define WRITE_nvr 0xa0
+// with NTSC
+#define ENABLE_display 0x08
+#define ENABLE_display_vert 0x0c
+#define MAX7456_reset 0x02
+#define DISABLE_display 0x00
+#define STATUS_reg_nvr_busy 0x20
+
+void write_NVM(uint8_t char_address)
+{
+#ifdef WRITE_TO_MAX7456
+  // disable display
+   digitalWrite(MAX7456SELECT,LOW);
+  spi_transfer(VM0_reg); 
+  //spi_transfer(DISABLE_display);
+
+  
+  
+  //digitalWrite(MAX7456SELECT,LOW);
+  //spi_transfer(VM0_reg);
+  spi_transfer(Settings[S_VIDEOSIGNALTYPE]?0x40:0);
+
+  spi_transfer(MAX7456ADD_CMAH); // set start address high
+  spi_transfer(char_address);
+
+  for(uint8_t x = 0; x < NVM_ram_size; x++) // write out 54 bytes of character to shadow ram
+  {
+    spi_transfer(MAX7456ADD_CMAL); // set start address low
+    spi_transfer(x);
+    spi_transfer(MAX7456ADD_CMDI);
+    spi_transfer(fontData[x]);
+  }
+
+  // transfer 54 bytes from shadow ram to NVM
+  spi_transfer(MAX7456ADD_CMM);
+  spi_transfer(WRITE_nvr);
+  
+  // wait until bit 5 in the status register returns to 0 (12ms)
+  while ((spi_transfer(MAX7456ADD_STAT) & STATUS_reg_nvr_busy) != 0x00);
+
+ spi_transfer(VM0_reg); // turn on screen next vertical
+  //spi_transfer(ENABLE_display_vert); 
+ spi_transfer(Settings[S_VIDEOSIGNALTYPE]?0x4c:0x0c);
+  digitalWrite(MAX7456SELECT,HIGH);  
+#else
+  delay(12);
+#endif
+}
