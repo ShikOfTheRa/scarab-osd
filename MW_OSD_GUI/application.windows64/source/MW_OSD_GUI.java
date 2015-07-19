9ffa600bb2de2bbca7cce664b97b65ea99950430
Anton Krug <antonkrug@gmail.com>
2015-07-19 10:26:30 +0100

    Absolute path and file name of saved font in the font editor fixed

--- MW_OSD_GUI/application.windows64/source/MW_OSD_GUI.java
+++ MW_OSD_GUI/application.windows64/source/MW_OSD_GUI.java
@@ -6043,7 +6043,7 @@ public void CreateFontFile(){
  //Output.println("done");
  Output.flush(); // Writes the remaining data to the file
   Output.close();  
-  img_Clear = LoadFont("/data/default.mcm");  
+  img_Clear = LoadFont("data/custom.mcm");  
 }
 
 
