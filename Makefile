bf94f3308cef3024ad4078834a6826b1c2720de5
nathan <nathan@vertile.com>
2016-04-22 23:31:12 -0700

    build tools, just run `make` on macosx, windows and linux code each need 1 line added and testing

---
+++ Makefile
@@ -0,0 +1,83 @@
+# Makefile taken from the dRonin project
+.DEFAULT_GOAL := all
+
+WHEREAMI := $(dir $(lastword $(MAKEFILE_LIST)))
+export ROOT_DIR := $(realpath $(WHEREAMI)/ )
+export BUILD_ALL_DEPENDENCIES := $(BUILD_ALL_DEPENDENCIES)
+# import macros common to all supported build systems
+include $(ROOT_DIR)/make/system-id.mk
+
+# configure some directories that are relative to wherever ROOT_DIR is located
+TOOLS_DIR := $(ROOT_DIR)/tools
+BUILD_DIR := $(ROOT_DIR)/build
+DL_DIR := $(ROOT_DIR)/downloads
+
+export RM := rm
+
+# import macros that are OS specific
+#include $(ROOT_DIR)/make/$(OSFAMILY).mk
+
+# include the tools makefile
+include $(ROOT_DIR)/make/tools.mk
+
+# Decide on a verbosity level based on the V= parameter
+export AT := @
+
+ifndef V
+export V0    :=
+export V1    := $(AT)
+else ifeq ($(V), 0)
+export V0    := $(AT)
+export V1    := $(AT)
+else ifeq ($(V), 1)
+endif
+
+$(DL_DIR):
+	mkdir -p $@
+
+$(TOOLS_DIR):
+	mkdir -p $@
+
+$(BUILD_DIR):
+	mkdir -p $@
+
+# instructions
+.PHONY: help
+help:
+	@echo
+	@echo "   This Makefile is tested on macosx by @nathantsoi, please updated this message if you're using another platform"
+	@echo "   Shamelessly copied from the dRonin project. Mad cred to them."
+	@echo
+	@echo "   Here is a summary of the available targets:"
+	@echo
+	@echo "   [Tool Installers]"
+	@echo "     arduino_builder_install      - Install the arduino_builder toolchain"
+	@echo
+	@echo "   [Firmware]"
+	@echo "     arduino                      - Build firmware for the arduino based MinimOsd"
+	@echo
+	@echo "   Hint: Add V=1 to your command line to see verbose build output."
+	@echo
+	@echo "   Note: All tools will be installed into $(TOOLS_DIR)"
+	@echo "         All build output will be placed in $(BUILD_DIR)"
+	@echo
+
+ARDUINO_DEVICE_TARGET := arduino:avr:pro:cpu=16MHzatmega328
+SRC_DIR := $(ROOT_DIR)/MW_OSD
+arduino: $(BUILD_DIR)
+arduino:
+	$(V1) $(ARDUINO_BUILDER_BIN) \
+		-hardware $(ARDUINO_APP_RESOURCE_DIR)/hardware \
+		-tools $(ARDUINO_APP_RESOURCE_DIR)/hardware/tools \
+		-tools $(ARDUINO_BUILDER_DIR)/tools \
+		-libraries $(ROOT_DIR)/MW_OSD \
+		-fqbn=$(ARDUINO_DEVICE_TARGET) \
+		-build-path $(BUILD_DIR) \
+		-verbose \
+		-compile \
+	  $(SRC_DIR)/MW_OSD.ino
+
+all: arduino
+
+clean:
+	$(V1) $(RM) -rf $(BUILD_DIR)/*
---
+++ make/tools.mk
@@ -0,0 +1,83 @@
+# Set up Arduino builder SDK
+ARDUINO_BUILDER_VERSION := 1.3.13
+ARDUINO_BUILDER_DIR := $(TOOLS_DIR)/arduino-builder-$(ARDUINO_BUILDER_VERSION)
+ARDUINO_BUILDER_BIN := $(TOOLS_DIR)/arduino-builder-$(ARDUINO_BUILDER_VERSION)/arduino-builder
+
+# Arduino builder depends on files found in the Arduino app download
+ARDUINO_APP_VERSION := 1.6.8
+ARDUINO_APP_DIR:= $(TOOLS_DIR)/arduino-app-$(ARDUINO_APP_VERSION)
+
+# from: https://github.com/arduino/arduino-builder/releases
+ifdef LINUX
+	# or linux64
+	ARDUINO_BUILDER_URL := https://github.com/arduino/arduino-builder/releases/download/$(ARDUINO_BUILDER_VERSION)/arduino-builder-$(OSFAMILY)32-$(ARDUINO_BUILDER_VERSION).tar.bz2
+	ARDUINO_APP_URL := https://downloads.arduino.cc/arduino-$(ARDUINO_APP_VERSION)-$(OSFAMILY).zip
+	$(error linux platform support: please set ARDUINO_APP_RESOURCE_DIR in make/tooks.mk to the directory containing the arduino hardware folder and remove this error message. see MACOSX section for an example)
+endif
+ifdef MACOSX
+	ARDUINO_BUILDER_URL := https://github.com/arduino/arduino-builder/releases/download/$(ARDUINO_BUILDER_VERSION)/arduino-builder-$(OSFAMILY)-$(ARDUINO_BUILDER_VERSION).tar.bz2
+	ARDUINO_APP_URL := https://downloads.arduino.cc/arduino-$(ARDUINO_APP_VERSION)-$(OSFAMILY).zip
+	ARDUINO_APP_RESOURCE_DIR := $(ARDUINO_APP_DIR)/Arduino.app/Contents/Java/
+endif
+ifdef WINDOWS
+	ARDUINO_BUILDER_URL := https://github.com/arduino/arduino-builder/releases/download/$(ARDUINO_BUILDER_VERSION)/arduino-builder-$(OSFAMILY)-$(ARDUINO_BUILDER_VERSION).zip
+	ARDUINO_APP_URL := https://downloads.arduino.cc/arduino-$(ARDUINO_APP_VERSION)-$(OSFAMILY).tgz
+	$(error windows platform support: please set ARDUINO_APP_RESOURCE_DIR in make/tooks.mk to the directory containing the arduino hardware folder. see MACOSX section for an example)
+endif
+
+.PHONY: arduino_app_install
+
+arduino_app_install: ARDUINO_APP_FILE := $(notdir $(ARDUINO_APP_URL))
+# order-only prereq on directory existance:
+arduino_app_install: | $(DL_DIR) $(TOOLS_DIR)
+arduino_app_install: arduino_app_clean
+ifneq ($(OSFAMILY), linux)
+  # download the source only if it's newer than what we already have
+ifneq ($(OSFAMILY), windows)
+	$(V1) wget --no-check-certificate -N -P "$(DL_DIR)" "$(ARDUINO_APP_URL)"
+else
+	$(V1) curl -L -k -o "$(DL_DIR)/$(ARDUINO_APP_FILE)" "$(ARDUINO_APP_URL)"
+endif
+	$(V1) unzip -q -d $(ARDUINO_APP_DIR) "$(DL_DIR)/$(ARDUINO_APP_FILE)"
+else
+	$(V1) curl -L -k -o "$(DL_DIR)/$(ARDUINO_APP_FILE)" "$(ARDUINO_APP_URL)"
+	$(V1) tar -C $(TOOLS_DIR) -xf "$(DL_DIR)/$(ARDUINO_APP_FILE)"
+endif
+
+.PHONY: arduino_app_clean
+arduino_app_clean:
+	$(V1) [ ! -d "$(ARDUINO_APP_DIR)" ] || $(RM) -r $(ARDUINO_APP_DIR)
+
+
+
+.PHONY: arduino_builder_install
+
+arduino_builder_install: arduino_app_install
+arduino_builder_install: ARDUINO_BUILDER_FILE := $(notdir $(ARDUINO_BUILDER_URL))
+# order-only prereq on directory existance:
+arduino_builder_install: | $(DL_DIR) $(TOOLS_DIR)
+arduino_builder_install: arduino_builder_clean
+ifneq ($(OSFAMILY), windows)
+	$(V1) wget --no-check-certificate -N -P "$(DL_DIR)" "$(ARDUINO_BUILDER_URL)"
+	$(V1) mkdir -p $(ARDUINO_BUILDER_DIR)
+	$(V1) tar -C $(ARDUINO_BUILDER_DIR) -xjf "$(DL_DIR)/$(ARDUINO_BUILDER_FILE)"
+else
+	$(V1) curl -L -k -o "$(DL_DIR)/$(ARDUINO_BUILDER_FILE)" "$(ARDUINO_BUILDER_URL)"
+	$(V1) unzip -q -d $(ARDUINO_BUILDER_DIR) "$(DL_DIR)/$(ARDUINO_BUILDER_FILE)"
+endif
+
+.PHONY: arduino_builder_clean
+arduino_builder_clean:
+	$(V1) [ ! -d "$(ARDUINO_BUILDER_DIR)" ] || $(RM) -r $(ARDUINO_BUILDER_DIR)
+
+# make sure we have the right dependencies installed
+ifneq ($(MAKECMDGOALS), arduino_app_install)
+ifeq ($(wildcard $(ARDUINO_APP_RESOURCE_DIR)),)
+  $(error please run: `make arduino_app_install`, missing "$(ARDUINO_APP_RESOURCE_DIR)", install the arduino app
+endif
+endif
+ifneq ($(MAKECMDGOALS), arduino_builder_install)
+ifeq ($(wildcard $(ARDUINO_BUILDER_DIR)),)
+  $(error please run: `make arduino_builder_install`, missing "$(ARDUINO_BUILDER_DIR)", install arduino builder)
+endif
+endif
