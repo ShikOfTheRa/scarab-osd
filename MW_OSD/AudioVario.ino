da07e14a94c8bd952219943846e8a6d441a9272e
ShikOfTheRa <gary@flypix.co.uk>
2017-07-20 14:33:42 +0100

    Prep for new tone generator

--- MW_OSD/AudioVario.ino
+++ MW_OSD/AudioVario.ino
@@ -6,6 +6,45 @@
   Modified by Shiki for MWOSD
 */
 
+/*
+uint8_t _pinMask = 0;         // Pin bitmask.
+volatile uint8_t *_pinOutput; // Output port register
+
+void NewTone(uint8_t pin, unsigned long frequency, unsigned long length) {
+  uint8_t prescaler = _BV(CS10);                 // Try using prescaler 1 first.
+  unsigned long top = F_CPU / frequency / 4 - 1; // Calculate the top.
+  if (top > 65535) {                             // If not in the range for prescaler 1, use prescaler 256 (61 Hz and lower @ 16 MHz).
+    prescaler = _BV(CS12);                       // Set the 256 prescaler bit.
+    top = top / 256 - 1;                         // Calculate the top using prescaler 256.
+  }
+
+  if (_pinMask == 0) {
+    _pinMask = digitalPinToBitMask(pin);                    // Get the port register bitmask for the pin.
+    _pinOutput = portOutputRegister(digitalPinToPort(pin)); // Get the output port register for the pin.
+    uint8_t *_pinMode = (uint8_t *) portModeRegister(digitalPinToPort(pin)); // Get the port mode register for the pin.
+    *_pinMode |= _pinMask; // Set the pin to Output mode.
+  }
+
+  ICR1    = top;                     // Set the top.
+  if (TCNT1 > top) TCNT1 = top;      // Counter over the top, put within range.
+  TCCR1B  = _BV(WGM13)  | prescaler; // Set PWM, phase and frequency corrected (ICR1) and prescaler.
+  TCCR1A  = _BV(COM1B0);
+  TIMSK1 |= _BV(OCIE1A);             // Activate the timer interrupt.
+}
+
+void noNewTone(uint8_t pin) {
+  TIMSK1 &= ~_BV(OCIE1A);   // Remove the timer interrupt.
+  TCCR1B  = _BV(CS11);      // Default clock prescaler of 8.
+  TCCR1A  = _BV(WGM10);     // Set to defaults so PWM can work like normal (PWM, phase corrected, 8bit).
+  *_pinOutput &= ~_pinMask; // Set pin to LOW.
+  _pinMask = 0; // Flag so we know note is no longer playing.
+}
+
+ISR(TIMER1_COMPA_vect) { // Timer interrupt vector.
+  *_pinOutput ^= _pinMask; // Toggle the pin state.
+}
+*/
+
 void AudioVarioUpdate()
 {
 #ifdef AUDIOVARIOSWITCH
@@ -34,10 +73,12 @@ void AudioVarioUpdate()
   if ((toneFreq < KKDEADBANDLOW) ||  ((toneFreq > KKDEADBANDHIGH)  && (ddsAcc > 0))) 
   {
     tone(KKAUDIOVARIO, toneFreq + 510);  
+//    NewTone(KKAUDIOVARIO, toneFreq + 510, 0);  
   }
   else
   {
     noTone(KKAUDIOVARIO);
+//    noNewTone(KKAUDIOVARIO);
   }
 }
 
