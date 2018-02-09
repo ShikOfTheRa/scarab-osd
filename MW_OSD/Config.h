6e35ab4a93f3dff97d714bd4acb031f5fc03cad6
ShikOfTheRa <gary@flypix.co.uk>
2018-02-09 16:28:12 +0000

    Renamed mavlink to ardupilot

--- MW_OSD/Config.h
+++ MW_OSD/Config.h
@@ -36,8 +36,8 @@
 //#define CLEANFLIGHT               // Uncomment this if you are using latest CLEANFLIGHT version from repository (2.2.0 at time of this MWOSD release)
 //#define iNAV                      // Uncomment this if you are using latest iNAV version from repository (1.01 at time of this MWOSD release)
 //#define iNAV_KK                   // Uncomment this if you are using AEROMAX OSD and BARO sensor addition with iNAV with KK audio vario
-//#define APM                       // Uncomment this if you are using Ardupilot or APM MAVLINK 1.0 compatible FC
-//#define PX4                       // Uncomment this if you are using PX4 stack
+//#define APM                       // Uncomment this if you are using Ardupilot on APM / PIXHAWK / other supported hardware. Supports most MAVLINK 1.0 compatible FC
+//#define PX4                       // Uncomment this if you are using PX4 stack on PIXHAWK and other supported hardware
 //#define BASEFLIGHT                // Uncomment this if you are using latest BASEFLIGHT version from repository (Stable 2015.08.27 at time of this MWOSD release)
 //#define MULTIWII                  // Uncomment this if you are using latest 2.4 MULTIWII
 //#define MAHOWII                   // Uncomment this if you are using MAHOWII (https://github.com/mahowik/mahowii)
