951ba35dd684f534227fc51e70305aa1ade95190
ShikOfTheRa <gary@flypix.co.uk>
2019-09-25 11:22:23 +0100

    >6s battery support  (Mavlink only)

--- MW_OSD/MW_OSD.ino
+++ MW_OSD/MW_OSD.ino
@@ -74,9 +74,9 @@ uint16_t UntouchedStack(void)
 #define PGMSTR(p) (char *)pgm_read_word(p)
 
 //------------------------------------------------------------------------
-#define MWVERS "MW-OSD - R1.9.1.3"
+#define MWVERS "MW-OSD - R1.9.1.4"
 //#define MWVERS "MW-OSD - R1.9"
-#define MWOSDVERSION 1913 // 1660=1.6.6.0 for GUI
+#define MWOSDVERSION 1914 // 1660=1.6.6.0 for GUI
 #define EEPROMVER 17      // for eeprom layout verification
 
 #include <avr/pgmspace.h>
